96e8c67a264a4c22ca3211b9f99f02124cabbb3f
pavan-3000 <pavansingh3000@gmail.com>
2024-11-29 11:23:45 +0530

    main prohefct

---
+++ build/precache-manifest.a4c4cac7b1f679a223002907d262c9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbe384459418cde8ac53bbe68a952f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa8dedacab7d82ee00c",
+    "url": "/static/js/2.45baba9d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.45baba9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d7ccb3f375b21641a9d",
+    "url": "/static/js/main.5dc12d7a.chunk.js"
+  },
+  {
+    "revision": "6560f3fcf71be0f2c6b8",
+    "url": "/static/js/runtime-main.7533f924.js"
+  },
+  {
+    "revision": "1360829ad46b812bafdb623d7ac2283f",
+    "url": "/static/media/7ec0559f-6180-4b29-becd-0f12f702ec5c._UR3840,1440_SX1440_FMjpg_.1360829a.jpeg"
+  }
+]);
